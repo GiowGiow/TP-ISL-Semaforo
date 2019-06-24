7dc25d14ea3b05a6490f57822ea718eb756fa7d4
GiowGiow <giofmartinelli@gmail.com>
2019-06-24 20:11:14 +0000

    Semaforo B funcionando, provavelmente...

--- semaforo.v
+++ semaforo.v
@@ -1,53 +1,111 @@
+`define true 1
+`define false 0
+
 module semaforo(input clk, input rst, input bt,
 	output reg [2:0] A, output reg [2:0] B);
 
-	//Insira seu codigo aqui
-	reg [1:0] state, nextState;
+	reg [1:0] state, next_state;
     reg [8:0] cnt;
 
-    // Assíncrono: Seguinte estado
+	reg [1:0] state_b;
+
+    reg start_b = `false;
+    reg ciclo = `false;
+    reg follow_a = `false;
+
+    // Assíncrono: seta o seguinte estado e reseta o contador
     always @(state, cnt) begin
       case(state)
-      0:  // Cor Verde
-        if (cnt == `VERDE) 
-        	begin
-        	  nextState = 1;
-         	  cnt = 0;
-        	end
-        else nextState = 0;
-      1:  // Cor Amarela
+      0: // Cor Verde
+	    if (cnt == `VERDE) 
+	    	begin
+          // Se B ja estiver seguindo A, coloque como falso o bt
+          if (follow_a) start_b = `false;
+          // Semaforo B para de seguir A quando completa o cilo
+	    	  follow_a = `false;
+	    	  // Ciclo foi completado
+          ciclo = `true;
+
+	    	  next_state = 1; // Prox estado
+	     	  cnt = 0; // Reseta o contador
+	    	end
+	    else next_state = 0;
+
+	  1: // Cor Amarela
+        begin
+        // Ciclo em andamento
+        ciclo = `false;
         if (cnt == `AMARELO) 
         	begin
-        	  nextState = 2;
+        	  next_state = 2;
         	  cnt = 0;
         	end
-        else nextState = 1;
-      2:// Cor Vermelha
+        else next_state = 1;
+        end
+      2: // Cor Vermelha
         if (cnt == `VERMELHO)
         	begin 
-        	  nextState = 0;
+        	  next_state = 0;
         	  cnt = 0;
         	end
-        else nextState = 2;
-      default: nextState = 0;
+        else next_state = 2;
+      
+      default: next_state = 0;
       endcase
     end
 
+    // Quando tiver um reset (borda de descida)
     always @(negedge rst) begin
-      cnt = 1;
-      state = 0;
+      cnt = 1; // Coloca contador em 1
+      state = 0; // Coloca estado em 0
+      state_b = 0;
+      start_b = `false;
     end
 
-    // Sincrona: Atualizacao do estado && Reset
+    // Quando botao for acionado
+    always @(posedge bt) begin
+    	start_b = `true;
+    end
+
+    // Sincrono: Atualizacao do estado
     always @(posedge clk) begin
-      state = nextState;
+      state = next_state;
+      // Se o botao foi apertado e comecou um ciclo entao
+      // comece a seguir o semaforo A
+      if(start_b == `true && ciclo == `true) begin
+        start_b = `false;
+        ciclo = `false;
+        follow_a = `true;
+      end
+      if(follow_a)
+      	state_b = state;
     end
 
     // Saidas
     always @(state) begin
-      if(state == 0) A = 3'b 100; // Cor Verde
-      else if(state == 1) A = 3'b 010; // Cor Amarela
-      else A = 3'b 001; // Cor Vermelha
+      if(state == 0) 
+      	begin
+      	A = 3'b 100; // Cor Verde
+      	end
+      else if(state == 1) begin
+        A = 3'b 010; // Cor Amarela
+        end
+      else begin
+        A = 3'b 001; // Cor Vermelha
+        end
+    end
+
+	always @(state_b) begin
+      if(state_b == 0) 
+      	begin
+      	B = 3'b 100; // Cor Verde
+      	end
+      else if(state_b == 1) begin
+        B = 3'b 010; // Cor Amarela
+        end
+      else begin
+        B = 3'b 001; // Cor Vermelha
+        end
     end
 
     // Contador	
